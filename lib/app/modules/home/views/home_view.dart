c635d0aad4b36cdade6171ef20deb9d160a3923b
duc <duc@appiphany.com.au>
2024-11-24 22:35:23 +0700

    add web

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/app/components/product_item.dart';
 import 'package:ecommerce_app/app/components/screen_title.dart';
 import 'package:ecommerce_app/app/modules/home/controllers/home_controller.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -26,7 +27,7 @@ class HomeView extends GetView<HomeController> {
                     crossAxisCount: 2,
                     crossAxisSpacing: 15.w,
                     mainAxisSpacing: 15.h,
-                    mainAxisExtent: 300.h,
+                    mainAxisExtent: kIsWeb ? 500.h : 300.h,
                   ),
                   shrinkWrap: true,
                   primary: false,
